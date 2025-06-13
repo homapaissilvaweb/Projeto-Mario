44a28254b403a3be5d95f6e36b61aeae756613f4
homapaissilvaweb <homapaissilva@gmail.com>
2025-06-13 20:44:01 -0300

    Alteração-java

--- script.js
+++ script.js
@@ -3,15 +3,15 @@
 const formulario = document.querySelector(".Formulario-fale-conosco")
 const mascara = document.querySelector(".mascara-2")
 
-function cliqueiNoBotao () { 
-formulario.style.left = "50%"
-formulario.style.transform = "translateX(-50%)"
-mascara.style.visibility = "visible"
+function cliqueiNoBotao() {
+    formulario.style.left = "50%"
+    formulario.style.transform = "translateX(-50%)"
+    mascara.style.visibility = "visible"
 
 }
 
-function CliqueiNaMascara () {
-   formulario.style.left = "-300px"
-formulario.style.transform = "translateX(0)"
-mascara.style.visibility = "hidden"
+function CliqueiNaMascara() {
+    formulario.style.left = "-300px"
+    formulario.style.transform = "translateX(0)"
+    mascara.style.visibility = "hidden"
 }
